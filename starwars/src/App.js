4681c533c64a472ff4ed02b17211b2143baa07f8
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-08-02 20:37:00 -0700

    Completed MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import styled from 'styled-components';
 
 import Cards from './components/Cards';
 
@@ -12,6 +13,12 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  `;
 
   useEffect(() => {
     axios
@@ -26,7 +33,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      {info.map(character => <Cards info={character} />)}
+      <Wrapper className="Wrapper">
+        {info.map(character => <Cards info={character} />)}
+      </Wrapper>
     </div>
   );
 }
--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: lightgray;
+  width: 300px;
+  border-radius: 30%;
+  margin-bottom: 20px;
+  border: black solid 1px;
+
+`;
 
 export default function Character({ info }) {
   return (
-    <div>
+    <Container>
       <h3>{info.name}</h3>
-      <p>Height: {info.height} </p>
-      <p>Mass: {info.mass} </p>
-      <p>Hair Color: {(info.hair_color)}</p>
-      <p>Skin Color: {(info.skin_color)}</p>
-      <p>Eye Color: {(info.eye_color)}</p>
-      <p>Birth Year: {info.birth_year}</p>
-      <p>Gender: {(info.gender)}</p>
-    </div>
+      <div>
+        <p>Height: {info.height} </p>
+        <p>Mass: {info.mass} </p>
+        <p>Hair Color: {(info.hair_color)}</p>
+        <p>Skin Color: {(info.skin_color)}</p>
+        <p>Eye Color: {(info.eye_color)}</p>
+        <p>Birth Year: {info.birth_year}</p>
+        <p>Gender: {(info.gender)}</p>
+      </div>
+    </Container>
   )
 }
+
+
+
